fe363866fa7b281cd5bcd04dfe2dd79fdbe4fe25
冠龍 李 <nionismine@yahoo.com.tw>
2021-12-07 16:58:34 +0800

    Add project files.

---
+++ Consumer1/Program.cs
@@ -0,0 +1,51 @@
+﻿using Confluent.Kafka;
+
+namespace KafkaDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group1",
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                Console.WriteLine("Please choose a Topic to receive:");
+                var topic = Console.ReadLine();
+                consumer.Subscribe(topic);
+
+                var tokenSource = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, eventArgs) =>
+                {
+                    eventArgs.Cancel = true;
+                    tokenSource.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var result = consumer.Consume(tokenSource.Token);
+                            Console.WriteLine($"Consumed message: {result.Message.Value} from topic {result.Topic} (partition:{result.Partition} / offset:{result.Offset})");
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Error occured: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    consumer.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,64 @@
+﻿using Confluent.Kafka;
+
+namespace KafkaDemo
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            string? topic, message;
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092",
+            };
+
+            Console.WriteLine("Please choose a Topic to deliver:");
+            topic = Console.ReadLine();
+
+            while (true)
+            {
+                Console.WriteLine("Please enter the Message:");
+                message = Console.ReadLine();
+
+                try
+                {
+                    using (var producer = new ProducerBuilder<Null, string>(config).Build())
+                    {
+                        var result = await producer.ProduceAsync(topic, new Message<Null, string> { Value = message });
+                        Console.WriteLine($"Delivered message: {result.Message.Value} to topic {result.Topic} (partition:{result.Partition} / offset:{result.Offset})");
+                    }
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+        }
+
+        public static void MainSecond(string[] args)
+        {
+            string? topic;
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092",
+            };
+
+            Console.WriteLine("Please choose a Topic to deliver:");
+            topic = Console.ReadLine();
+
+            Action<DeliveryReport<Null, string>> handler = report =>
+                Console.WriteLine(!report.Error.IsError
+                    ? $"Delivered message to {report.TopicPartitionOffset}"
+                    : $"Delivery Error: {report.Error.Reason}");
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                for (int i = 0; i < 100; ++i)
+                {
+                    producer.Produce(topic, new Message<Null, string> { Value = i.ToString() }, handler);
+                }
+                producer.Flush(TimeSpan.FromSeconds(10));
+            }
+        }
+    }
+}
